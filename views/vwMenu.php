42787107c87c90aafabc9de2740cca895460984c
David Soriano <davidscicua314@gmail.com>
2024-03-23 21:00:42 -0500

    Conexion de vistas y creacion de codocumento para vistas

--- views/vwMenu.php
+++ views/vwMenu.php
@@ -1,6 +1,6 @@
 <nav>
     <ul>
-        <li class="nav-item"><a class="nav-link" href="index.php?pg=1001">Inicio</a></li>
+        <li class="nav-item"><a class="nav-link" href="views/vwLogin.php">Inicio</a></li>
         <li class="nav-item"><a class="nav-link" href="index.php?pg=1002">Nosotros</a></li>
         <li class="nav-item"><a class="nav-link" href="index.php?pg=1003">Productos</a></li>
     </ul>

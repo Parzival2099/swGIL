62d823e729c4079c044411f8433f8c8ce186522e
davidscicua314@gmail.com <davidscicua314@gmail.com>
2024-03-26 20:40:04 -0500

    avance vista producto ind

--- views/vwInfoPrd.php
+++ views/vwInfoPrd.php
@@ -1,13 +1,21 @@
 <div class="container-fluid d-flex justify-content-center align-items-center flex-column">
-    <div class="row bx-cont-inf d-flex gap-5 align-items-center justify-content-center">
+    <div class="row bx-cont-inf d-flex justify-content-center">
         <section class="col-6 section">
-            <div class="row">
+            <div class="row bx-fot-art">
                 <div class="col-2">
                     <div class="row d-flex flex-column min-img gap-5">
-                        <div class="col"></div>
-                        <div class="col"></div>
-                        <div class="col"></div>
-                        <div class="col"></div>
+                        <div class="col">
+                            <img src="IMG/Articulo 5.webp" alt="">
+                        </div>
+                        <div class="col">
+                            <img src="IMG/min-art5.webp" alt="">
+                        </div>
+                        <div class="col">
+                            <img src="IMG/min1-art5.webp" alt="">
+                        </div>
+                        <div class="col">
+                            <img src="IMG/min2-art5.webp" alt="">
+                        </div>
                     </div>
                 </div>
                 <div class="col-10 bx-art-pr">
@@ -15,10 +23,31 @@
                 </div>
             </div>
         </section>
-        <aside class="aside col-5">
+        <aside class="aside col-6 bx-txt-prd">
             <div class="row">
-                <div class="col-7">
-                    <h3>Monitor Curvo SAMSUNG 27" QLED</h3>
+                <div class="col-7 bx-car-prd">
+                    <p>Nuevo | +1000 vendidos</p>
+                    <h3>Monitor Curvo SAMSUNG 32"</h3>
+                    <del>$919.000</del>
+                    <h4>$859.900</h4>
+                    <section>
+                        <p>
+                            Caracteristicas principales:
+                        </p>
+                        <ul>
+                            <li>Voltaje: 100V/240V</li>
+                            <li>Tamaño de la pantalla: 32 "</li>
+                            <li>Curvo</li>
+                            <li>Tiene una resolución de 1920px-1080px.</li>
+                            <li>Relación de aspecto de 16:9.</li>
+                            <li>Panel VA.</li>
+                            <li>Su brillo es de 300cd/m²</li>
+                            <li>Con conexión VGA/D-Sub</li>
+                            <li>Es reclinable.</li>
+                            <li>Comodidad visual en todo momento.</li>
+                        </ul>
+                        <a href="#">Ver más detalles</a>
+                    </section>
                 </div>
                 <div class="col-5"></div>
             </div>

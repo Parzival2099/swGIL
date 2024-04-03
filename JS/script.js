e632395adbce459190abf43307d01ccd91fbeb93
David Soriano <davidscicua314@gmail.com>
2024-04-03 11:42:56 -0500

    ajustes

--- JS/script.js
+++ JS/script.js
@@ -3,3 +3,23 @@ function leerCorreo(id1, id2, id3) {
     document.getElementById(id2).innerHTML = correo;
     document.getElementById(id3).innerHTML = correo;
 }
+
+function ocultarBx() {
+    let btnPrf = document.getElementById("btnPrf");
+    let bandera = false;
+    if (bandera == false) {
+        btnPrf.addEventListener('click', () => {
+            document.getElementById('bx-opc-prf').style.height = '16%';
+            bandera = true;
+            if (bandera == true) {
+                btnPrf.addEventListener('click', () => {
+                    document.getElementById('bx-opc-prf').style.height = '0';
+                    bandera = false;
+                    ocultarBx()
+                })
+            }
+        })
+    }
+}
+
+ocultarBx();

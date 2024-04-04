07ed291ef128c3e4cb1f8c964fdeee4f74a9540f
Parzival2099 <165744735+Parzival2099@users.noreply.github.com>
2024-04-03 21:38:29 -0500

    sub

--- views/vwsubirprod.php
+++ views/vwsubirprod.php
@@ -1,9 +1,30 @@
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: rgb(8, 57, 102);">
+    <div class="container px-4">
+      <a class="navbar-brand" href="/">
+        <span style="color:#ffffff; font-size:26px; font-weight:bold; letter-spacing: 1px;">Logo</span>
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span
+          class="navbar-toggler-icon"></span></button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item"><a class="nav-link" href="/">Inicio</a></li>
+          <li class="nav-item"><a class="nav-link" href="/code">Ayuda</a></li>
+          <li class="nav-item"><a class="nav-link" href="/about">Acerca de la página</a></li>
+          <li class="nav-item"><a class="nav-link" href="/faq">Contactenos</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
     integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
     </script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
     integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
     </script>
+</body>
 <div class="container mt-5">
         <h1>Subir Producto</h1>
         <form>

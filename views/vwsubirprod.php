3ca3a6e7096a8103daec1dfa8dae3081016cb94b
Parzival2099 <165744735+Parzival2099@users.noreply.github.com>
2024-04-03 22:02:33 -0500

    sub

--- views/vwsubirprod.php
+++ views/vwsubirprod.php
@@ -24,5 +24,3 @@
             <button type="submit" class="btn btn-primary">Subir</button>
         </form>
     </div>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>

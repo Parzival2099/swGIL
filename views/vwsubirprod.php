5247306f918c655b82af6cf40a43cff0988e0780
Parzival2099 <165744735+Parzival2099@users.noreply.github.com>
2024-04-03 21:42:51 -0500

    sub

--- views/vwsubirprod.php
+++ views/vwsubirprod.php
@@ -1,3 +1,4 @@
+</head>
 <body>
   <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: rgb(8, 57, 102);">
     <div class="container px-4">
@@ -25,7 +26,10 @@
     integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
     </script>
 </body>
-<div class="container mt-5">
+</html>
+</head>
+<body>
+    <div class="container mt-5">
         <h1>Subir Producto</h1>
         <form>
             <div class="form-group">
@@ -53,3 +57,5 @@
     </div>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</body>
+</html>
